84ddd3ab6a234d876ce4280e4e4caeac186aa727
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-02-21 16:23:50 +0300

    jedis session storage is improved

--- hootka-core/src/main/kotlin/io/zensoft/web/api/internal/http/JedisSessionStorage.kt
+++ hootka-core/src/main/kotlin/io/zensoft/web/api/internal/http/JedisSessionStorage.kt
@@ -14,15 +14,26 @@ import java.util.*
 @Component
 class JedisSessionStorage(
     private val cookieName: String,
+    private val cookieExpiry: Long,
     private val jedis: Jedis
 ) : SessionStorage {
 
-    override fun findSession(id: String): HttpSession
-        = deserialize(HexBin.decode(jedis.get(id))) as HttpSession
+    private var timestamp: Long = 0
+
+
+    override fun findSession(id: String): HttpSession? {
+        if (!isRelevant()) {
+            jedis.del(id)
+            return null
+        }
+        return deserialize(HexBin.decode(jedis.get(id))) as HttpSession
+    }
 
     override fun createSession(): HttpSession {
         val sessionId = UUID.randomUUID().toString()
         val session = DefaultHttpSession(sessionId)
+        timestamp = System.currentTimeMillis()
+
         jedis.set(sessionId, HexBin.encode(serialize(session)))
         return session
     }
@@ -44,5 +55,7 @@ class JedisSessionStorage(
             jedis.del(it)
         }
     }
+
+    private fun isRelevant(): Boolean = ((System.currentTimeMillis() - timestamp) / 1000) <= cookieExpiry
 
 }
