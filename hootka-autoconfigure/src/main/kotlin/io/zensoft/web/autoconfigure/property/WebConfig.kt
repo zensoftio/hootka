e92f145a089c864f4388f1eb3bcc8dea0c7893fa
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-02-22 10:19:52 +0300

    jedis config is improved

--- hootka-autoconfigure/src/main/kotlin/io/zensoft/web/autoconfigure/property/WebConfig.kt
+++ hootka-autoconfigure/src/main/kotlin/io/zensoft/web/autoconfigure/property/WebConfig.kt
@@ -3,7 +3,6 @@ package io.zensoft.web.autoconfigure.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 import org.springframework.validation.annotation.Validated
-import redis.clients.jedis.Jedis
 
 @ConfigurationProperties(value = "hootka", ignoreUnknownFields = true)
 @Validated
@@ -19,11 +18,12 @@ class WebConfig(
 class SessionConfig(
     var cookieName: String = "session_id",
     var cookieMaxAge: Long = 1800,
-    val jedis: Jedis = JedisConfig().jedis
+    val redis: JedisConfig = JedisConfig()
 )
 
 class JedisConfig(
-    val jedis: Jedis = Jedis("localhost", 6379)
+    var host: String = "localhost",
+    var port: Int = 6379
 )
 
 class SecurityConfig(
